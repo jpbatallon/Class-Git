7b0f36062ba0e9102148e855b315caf72095f5f4
Juan Pablo Batallón <jpb_k13@hotmail.com>
2024-05-11 16:53:10 -0300

    Mi primer commit

---
+++ Leccion1/Ejercicio1.py
@@ -0,0 +1,9 @@
+#Califica tu día
+dia = input("¿Cómo estuvo tu día? (del 1 al 10)")
+print("Mi día estuvo de",dia)
+
+
+#Información sobre un libro
+libro = input("Proporciona el título del libro: ")
+autor = input("Proporciona el autor del libro: ")
+print(libro,"fue escrito por",autor)
