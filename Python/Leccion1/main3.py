05b5b5b252dddc5e7e341e464f94c461790f1547
Juan Pablo Batallón <jpb_k13@hotmail.com>
2024-05-12 11:53:29 -0300

    Se agrega main3 con los operadores aritméticos

---
+++ Python/Leccion1/main3.py
@@ -0,0 +1,33 @@
+#Operadores aritméticos
+
+#Suma
+num1 = 8
+num2 = 7
+
+suma = num1 + num2
+# print("El resultado de la suma es:",suma)
+print(f"El resultado de la suma es: {suma}")
+
+#Resta
+resta = num1 - num2
+print(f"El resultado de la resta es: {resta}")
+
+#Multiplicación
+multiplicacion = num1 * num2
+print(f"El resultado de la multiplicación es: {multiplicacion}")
+
+#División
+division = num1 / num2
+print(f"El resultado de la división es: {division}")
+
+#División tipo entero
+division = num1 // num2
+print(f"El resultado de la división tipo entera es: {division}")
+
+#Exponente
+exponente = num1 ** num2
+print(f"El resultado del exponente es: {exponente}")
+
+#Módulo o residuo
+modulo = num1 % num2
+print(f"El resultado del módulo o residuo es: {modulo}")
