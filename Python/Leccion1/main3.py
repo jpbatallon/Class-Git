f30972688dffcf1dd57b65ae4755b3a262a907e5
Juan Pablo Batallón <jpb_k13@hotmail.com>
2024-05-12 20:03:26 -0300

    Se agregó ejemplos de operadores.

--- Python/Leccion1/main3.py
+++ Python/Leccion1/main3.py
@@ -31,3 +31,79 @@ print(f"El resultado del exponente es: {exponente}")
 #Módulo o residuo
 modulo = num1 % num2
 print(f"El resultado del módulo o residuo es: {modulo}")
+
+
+#Operadores de asignación y comparación
+
+#Asignación
+miVariable = 10
+print(miVariable)
+
+#Reasignación
+miVariable = miVariable + 1
+print(miVariable)
+
+miVariable += 1  #Sintáxis
+print(miVariable)
+
+miVariable = miVariable - 1
+print(miVariable)
+
+miVariable -= 1  #Sintáxis
+print(miVariable)
+
+miVariable = miVariable * 1
+print(miVariable)
+
+miVariable *= 1  #Sintáxis
+print(miVariable)
+
+miVariable = miVariable / 2
+print(miVariable)
+
+miVariable /= 2  #Sintáxis
+print(miVariable)
+
+
+#Comparación
+a = 5
+b = 3
+resultado = a == b
+print(resultado)
+
+#Diferente
+resultado = a != b
+print(resultado)
+
+#Mayor que
+resultado = a > b
+print(resultado)
+
+#Mayor o igual que
+resultado = a >= b
+print(resultado)
+
+#Menor que
+resultado = a < b
+print(resultado)
+
+#Menor o igual que
+resultado = a <= b
+print(resultado)
+
+
+#Operadores lógicos
+
+#Operador and
+a = True
+b = True
+resultado = a and b
+print(resultado)
+
+#Operador or
+resultado = a or b
+print(resultado)
+
+#Operador not
+resultado = not a
+print(resultado)
