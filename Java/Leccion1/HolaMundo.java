b4e66d081eb070b14876f44039bc897dcbf889f1
Juan Pablo Batallón <jpb_k13@hotmail.com>
2024-06-03 15:51:40 -0300

    Se ingresa la carpeta Java al commit

---
+++ Java/Leccion1/HolaMundo.java
@@ -0,0 +1,166 @@
+package Leccion1;
+
+import java.util.Scanner;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+       /* System.out.println("Hola Mundo desde Java");
+
+        int miVariable = 10;
+        System.out.println(miVariable);
+        miVariable = 5;
+        System.out.println(miVariable);
+
+        String miVariableCadena = "Bienvenidos.";
+        System.out.println(miVariableCadena);
+        miVariableCadena = "Sigamos creciendo en programación.";
+        System.out.println(miVariableCadena);
+*/
+/*
+        var miVariableEntera2 = 10;
+        var miVariableCadena2 = "Seguimos estudiando";
+        System.out.println("miVariableEntera2 = " + miVariableEntera2);
+        System.out.println("miVariableCadena2 = " + miVariableCadena2);
+
+        var usuario = "Pedro";
+        var titulo = "Licenciado";
+        var union = titulo + " " + usuario;
+        System.out.println("union = " + union);
+        
+        var a = 7;
+        var b = 9;
+        System.out.println(usuario + (a + b));
+*/
+/*
+        var nombre = "Roberto";
+        System.out.println("\nNueva línea: \n" + nombre);
+        System.out.println("Tabulador: \t" + nombre);
+        System.out.println("Tabulador x2: \t\t" + nombre);
+        System.out.println("Retroceso: \b" + nombre);
+        System.out.println("Comillas simples: \'" + nombre + "\'");
+        System.out.println("Comillas dobles: \"" + nombre + "\"");
+*/
+/*
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite su nombre: ");
+        var usuario2 = entrada.nextLine();
+        System.out.println("usuario2 = " + usuario2);
+        System.out.println("Escriba su título o profesión: ");
+        var titulo2 = entrada.nextLine();
+        System.out.println("Resultado: " + titulo2 + " " + usuario2);
+
+        System.out.println("Escriba el título del libro: ");
+        var titulo = entrada.nextLine();
+        System.out.println("Escriba el nombre del autor: ");
+        var autor = entrada.nextLine();
+        System.out.println(titulo + " fue escrito por " + autor);
+ */
+/*
+        byte numeroEnteroByte = 127;
+        System.out.println("numeroEnteroByte = " + numeroEnteroByte);
+        System.out.println("Valor mínimo del Byte: " + Byte.MIN_VALUE);
+        System.out.println("Valor máximo del Byte: " + Byte.MAX_VALUE);
+
+        short numeroEnteroShort = 32767;
+        System.out.println("numeroEnteroShort = " + numeroEnteroShort);
+        System.out.println("Valor mínimo del Short: " + Short.MIN_VALUE);
+        System.out.println("Valor máximo del Short: " + Short.MAX_VALUE);
+
+        int numeroEnteroInt = 2147483647;
+        System.out.println("numeroEnteroInt = " + numeroEnteroInt);
+        System.out.println("Valor mínimo del Int: " + Integer.MIN_VALUE);
+        System.out.println("Valor máximo del Int: " + Integer.MAX_VALUE);
+
+        long numeroEnteroLong = 9223372036854775807L;
+        System.out.println("numeroEnteroLong = " + numeroEnteroLong);
+        System.out.println("Valor mínimo del Long: " + Long.MIN_VALUE);
+        System.out.println("Valor máximo del Long: " + Long.MAX_VALUE);
+*/
+/*
+        float numFloat = 3.4028235E38F;
+        System.out.println("numFloat = " + numFloat);
+        System.out.println("El número mínimo de Float es: " + Float.MIN_VALUE);
+        System.out.println("El número máximo de Float es: " + Float.MAX_VALUE);
+
+        double numDouble = 1.7976931348623157E308;
+        System.out.println("numDouble = " + numDouble);
+        System.out.println("El número mínimo de Double es: " + Double.MIN_VALUE);
+        System.out.println("El número máximo de Double es: " + Double.MAX_VALUE);
+*/
+/*
+        var numEntero = 20;
+        System.out.println("numEntero = " + numEntero);
+        var numFloat = 10.0;
+        System.out.println("numFloat = " + numFloat);
+        var numDouble = 10.0;
+        System.out.println("numDouble = " + numDouble);
+
+*/
+/*
+        char miVariableChar = 'a';
+        System.out.println("miVariableChar = " + miVariableChar);
+
+        char varCaracter = '\u0024';
+        System.out.println("varCaracter = " + varCaracter);
+        char varCaracterDecimal = 36;
+        System.out.println("varCaracterDecimal = " + varCaracterDecimal);
+        char varCaracterSimbolo = '$';
+        System.out.println("varCaracterSimbolo = " + varCaracterSimbolo);
+
+        var varCaracter2 = '\u0024';
+        System.out.println("varCaracter2 = " + varCaracter2);
+        var varCaracterDecimal2 = (char)36;
+        System.out.println("varCaracterDecimal2 = " + varCaracterDecimal2);
+        var varCaracterSimbolo2 = '$';
+        System.out.println("varCaracterSimbolo2 = " + varCaracterSimbolo2);
+
+        int varEnteroChar = '$';
+        System.out.println("varEnteroChar = " + varEnteroChar);
+        int caracterChar = 'B';
+        System.out.println("caracterChar = " + caracterChar);
+*/
+/*
+        boolean varBool = true;
+        System.out.println("varBool = " + varBool);
+
+        if(varBool) {
+            System.out.println("La bandera es verde.");
+        }
+        else{
+                System.out.println("La bandera es roja.");
+            }
+
+        var edad = 33;
+        if (edad >=18){
+            System.out.println("Eres mayor de edad.");
+        }
+        else{
+            System.out.println("Eres menor de edad.");
+        }
+*/
+/*
+        var edad = Integer.parseInt("20");
+        System.out.println("edad= " + (edad + 1));
+        var valorPI = Double.parseDouble("3.1416");
+        System.out.println("doublePI = " + valorPI);
+
+        var entrada = new Scanner(System.in);
+        System.out.println("Escriba su edad: ");
+        edad = Integer.parseInt(entrada.nextLine());
+        System.out.println("Edad: " + edad);
+*/
+/*
+        var edadTexto = String.valueOf(10);
+        System.out.println("edadTexto = " + edadTexto);
+
+        var fraseChar = "HolaMundo".charAt(4);
+        System.out.println("fraseChar = " + fraseChar);
+
+        var entrada = new Scanner(System.in);
+        System.out.println("Escriba un caracter: ");
+        fraseChar = entrada.nextLine().charAt(0);
+        System.out.println("fraseChar = " + fraseChar);
+*/
+        
+    }
+}
---
+++ Java/out/production/Java/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+G
+Leccion1/HolaMundo.java,5\a\5a80ebd4ae99f6a48aa56de82181fdbdffed5d9b
